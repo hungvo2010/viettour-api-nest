b341409864cf2490fd161921db449d39ce6f3a62
Vo Chanh Hung <62180150+hungvo2010@users.noreply.github.com>
2023-10-09 13:43:05 +0700

    Update change

---
+++ src/auth/auth.constant.ts
@@ -0,0 +1,4 @@
+export const ACCOUNT_ALREADY_EXIST =
+  "An account with this email already exists";
+export const CURRENT_PASSWORD_WRONG = "Current password is wrong";
+export const WRONG_CREDENTIALS = "Your email or password is wrong";
