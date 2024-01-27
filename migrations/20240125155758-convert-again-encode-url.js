aa51d6a6687f10ec17217d86e4e0c0c6e9c073a9
hungvo2010 <hungvochanhdp1@gmail.com>
2024-01-27 23:20:00 +0700

    add more migrations

--- migrations/20240125155758-convert-again-encode-url.js
+++ migrations/20240125155758-convert-again-encode-url.js
@@ -1,13 +1,15 @@
 module.exports = {
     up: async (db, client) => {
         await db.collection('Tour').updateMany(
-            {},
             {
-                $set: {
-                    encodeUrl: '/tour/' + '$encodeUrl',
-                },
+                "encodeUrl": { $exists: true }
             },
-        )
+            [{
+                $set: {
+                    encodeUrl: { $concat: ['/tour/', '$encodeUrl'] },
+                },
+            }]
+        );
     },
     down: async (db, client) => {
         // TODO write the statements to rollback your migration (if possible)
