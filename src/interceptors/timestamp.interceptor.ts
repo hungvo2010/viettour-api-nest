05b06cc6cc84f4368d5ca147342eb9063032bd83
hung.vo4 <hung.vo4@mservice.com.vn>
2024-02-27 00:08:09 +0700

    ok

--- src/interceptors/timestamp.interceptor.ts
+++ src/interceptors/timestamp.interceptor.ts
@@ -1,18 +1,18 @@
-import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { Observable, map } from 'rxjs';
+  import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+  import { Observable, map } from 'rxjs';
 
-export class TimestampInterceptor implements NestInterceptor {
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler<any>,
-  ): Observable<any> | Promise<Observable<any>> {
-    return next.handle().pipe(
-      map((data) => {
-        return {
-          ...data,
-          timestamp: new Date().toISOString(),
-        };
-      }),
-    );
+  export class TimestampInterceptor implements NestInterceptor {
+    intercept(
+      context: ExecutionContext,
+      next: CallHandler<any>,
+    ): Observable<any> | Promise<Observable<any>> {
+      return next.handle().pipe(
+        map((data) => {
+          return {
+            ...data as object,
+            timestamp: new Date().toISOString(),
+          };
+        }),
+      );
+    }
   }
-}
